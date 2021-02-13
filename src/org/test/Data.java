1fc04e2e3208884a79985970ebac13a8c634cfba
Ramanathan04 <a.ramanathan04@gmail.com>
2021-02-13 06:11:45 +0530

    Completed..

---
+++ src/org/test/Data.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Data {
+	public void t1() {
+		System.out.println("DONE");
+	}
+	public static void main(String []args) {
+	Data d=new Data();
+	d.t1();
+	}
+
+
+}
